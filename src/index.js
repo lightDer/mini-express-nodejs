9a30b6155dccb18094c398c81d6bdc3cd413940a
Peter <peterg@softmobile.com.tw>
2021-06-07 14:59:53 +0800

    add example

--- src/index.js
+++ src/index.js
@@ -11,10 +11,13 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(myLogger)
 
+app.get('/', (req, res) => {
+  res.send('Hello express.js')
+})
 app.get('/status', (req, res) => res.send('I am still alive.'))
 app.post('/mirror', (req, res) => res.json(req.body))
 
-app.get('*', (req, res) => res.status(404).json({ message: '404 Not found.' }))
+app.all('*', (req, res) => res.status(404).json({ message: '404 Not found.' }))
 
 app.listen(3000, () => {
   console.log('Sever listen on port 3000')
