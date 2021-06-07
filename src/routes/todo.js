019f4a0a38ee7d75c0788b9e90a5a2448c89c030
Peter <peterg@softmobile.com.tw>
2021-06-07 17:18:56 +0800

    add example

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -13,4 +13,9 @@ router.post('/api/todo', auth, addTodo)
 router.put('/api/todo/:id', auth, updateTodo)
 router.delete('/api/todo/:id', auth, deleteTodo)
 
+// router.get('/api/todos', getTodos)
+// router.post('/api/todo', addTodo)
+// router.put('/api/todo/:id', updateTodo)
+// router.delete('/api/todo/:id', deleteTodo)
+
 export default router
--- src/startups/db.js
+++ src/startups/db.js
@@ -3,6 +3,7 @@ import config from 'config'
 
 const myKnex = knex({
   client: 'sqlite3',
+  // debug: true,
   connection: {
     filename: config.get('db.filename')
   }
